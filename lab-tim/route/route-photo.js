7b84ec8b28f7de8a12b1a9b8d75ba790fb841c02
Tim Turner <timmyturner@me.com>
2017-09-13 16:31:59 -0700

    adds photo route and stuff

--- lab-tim/route/route-photo.js
+++ lab-tim/route/route-photo.js
@@ -15,20 +15,40 @@ module.exports = function(router) {
     console.log(req.file);
 
     return Photo.upload(req)
-    .then(photoData => new Photo(photoData).save())
-    .then(photo => res.json(photo))
-    .catch(err => errorHandler(err, req, res));
+      .then(photoData => new Photo(photoData).save())
+      .then(photo => res.json(photo))
+      .catch(err => errorHandler(err, req, res));
   });
+
   router.get('/api/photo/:_id', bearerAuth, (req, res) => {
+    debug('GET /api/photo/:_id');
 
+    return Photo.findById(req.params._id)
+      .then(photo => res.json(photo))
+      .catch(err => errorHandler(err, req, res));
   });
+
   router.get('/api/photo', bearerAuth, (req, res) => {
+    debug('GET /api/photo');
 
+    return Photo.find()
+      .then(photo => res.json(photo.map(photo => photo._id)))
+      .catch(err => errorHandler(err, req, res));
   });
+
   router.put('/api/photo/:_id', bearerAuth, upload.single('image'), (req, res) => {
+    debug('PUT /api/photo/:_id');
 
+    return Photo.findByIdAndUpdate(req.params._id, req.body, { upsert:true, runValidators:true })
+      .then(() => res.sendStatus(204))
+      .catch(err => errorHandler(err, req, res));
   });
-  router.delete('/api/photo/:_id', bearerAuth, (req, res) => {
 
+  router.delete('/api/photo/:_id', bearerAuth, (req, res) => {
+    debug('DELETE /api/photo/:_id');
+
+    return Photo.findByIdAndRemove(req.params._id)
+      .then(() => res.sendStatus(204))
+      .catch(err => errorHandler(err, req, res));
   });
 };
