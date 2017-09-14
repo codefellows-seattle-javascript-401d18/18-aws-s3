e1d2b2925e30b01cb850ea5d49e1f5625018e934
Madeline Stevens <stevensm1987@gmail.com>
2017-09-13 23:52:45 -0700

    AWS key, routes, started on tests, readme is updated, but haven't done any actual testing of things

--- lab-maddy/__test__/route/gallery.test.js
+++ lab-maddy/__test__/route/gallery.test.js
@@ -3,7 +3,7 @@
 const faker = require('faker');
 const mocks = require('../lib/mocks');
 const server = require('../../lib/server');
-const Gallery = require('../../model/gallery')
+const Gallery = require('../../model/gallery');
 const superagent = require('superagent');
 require('jest');
 
@@ -22,19 +22,19 @@ describe('Testing Gallery Routes', function() {
         .then(() => {
           return superagent.post(':4444/api/gallery')
             .set('Authorization', `Bearer ${this.userData.token}`)
-            .send(this.galleryFake)
+            .send(this.galleryFake);
         })
         .then(res => this.res = res);
-    })
+    });
 
     describe('Valid Requests', () => {
       test('should return a status 200', () => {
         expect(this.res.status).toBe(200);
-      })
+      });
       test('should return the new gallery resource', () => {
         expect(this.res.body.name).toBe(this.galleryFake.name);
         expect(this.res.body.desc).toBe(this.galleryFake.desc);
-      })
+      });
       test('should have a userId prop', () => {
         expect(this.res.body).toHaveProperty('userId');
       });
@@ -70,33 +70,47 @@ describe('Testing Gallery Routes', function() {
     });
   });
 
+  // * `GET` - test **200**, for a request made with a valid id
+  // * `GET` - test **401**, if no token was provided
+  // * `GET` - test **404**, for a valid request with an id that was not found
+
   describe('GET', function() {
     describe('Valid Requests', () => {
-
-    })
+      test('should return a status 200', () => {
+        expect(this.res.status).toBe(200);
+      }
+    }
 
     describe('Invalid Requests', () => {
+      test('should return a status 401, if no token was provided', () => {
+        return
+    });
 
-    })
-  })
+      test('should return a status 404, for a valid request with an id that was not found' , () => {
+  });
+
+  // * `PUT` - test **200**, for a post request with a valid body
+  // * `PUT` - test **401**, if no token was provided
+  // * `PUT` - test **400**, if the body was invalid
+  // * `PUT` - test **404**, for a valid request made with an id that was not found
 
   describe('PUT', function() {
     describe('Valid Requests', () => {
 
-    })
+    });
 
     describe('Invalid Requests', () => {
 
-    })
-  })
+    });
+  });
 
   describe('DELETE', function() {
     describe('Valid Requests', () => {
 
-    })
+    });
 
     describe('Invalid Requests', () => {
 
-    })
-  })
-})
+    });
+  });
+});
